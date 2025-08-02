2873482ba14953282d44c824d972b6a9abfe6be2
Himanshu Suthar <himanshusuthars810@gmail.com>
2025-08-02 14:09:35 +0530

    connected to backend

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,7 +1,7 @@
 // src/utils/api.js
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000/api";
+const API_BASE_URL = "https://backend-1-zqqy.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
